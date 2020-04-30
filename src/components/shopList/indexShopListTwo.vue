67d7675d3fd4b84b7f226851c80832fa9d2ca165
Wuyx <wuyuxin@jixingkeji.net>
2020-04-30 17:25:52 +0800

    提交页面

--- src/components/shopList/indexShopListTwo.vue
+++ src/components/shopList/indexShopListTwo.vue
@@ -2,6 +2,7 @@
   <div class="indexShopListTwo">
     <ul class="shoplisttwo_ul">
       <li class="ul_li">
+        <router-link to="#">
         <img class="shop_title_img" src="@/assets/image/shoplist1.png">
         <p class="shop_title">
           味它小型犬博美狗粮成犬幼犬粮全犬期狗粮 2.5kg
@@ -10,16 +11,19 @@
           <span class="price">￥44.9<em class="proce_em">满减</em></span>
           <span class="similarity">看相似</span>
         </div>
+        </router-link>
       </li>
       <li class="ul_li">
-        <img class="shop_title_img" src="@/assets/image/shoplist1.png">
-        <p class="shop_title hide_more">
-          味它小型犬博美狗粮成犬幼犬粮全犬期狗粮 2.5kg味它小型犬博美狗粮成犬幼犬粮全犬期狗粮 2.5kg
-        </p>
-        <div class="price_similarity">
-          <span class="price">￥44.9</span>
-          <span class="similarity">看相似</span>
-        </div>
+        <router-link to="#">
+          <img class="shop_title_img" src="@/assets/image/shoplist1.png">
+          <p class="shop_title hide_more">
+            味它小型犬博美狗粮成犬幼犬粮全犬期狗粮 2.5kg味它小型犬博美狗粮成犬幼犬粮全犬期狗粮 2.5kg
+          </p>
+          <div class="price_similarity">
+            <span class="price">￥44.9</span>
+            <span class="similarity">看相似</span>
+          </div>
+        </router-link>
       </li>
     </ul>
   </div>
